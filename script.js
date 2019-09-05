1ba1d6e818555f676b6d18c472628b36deb61132
Carlos Tamayo <carlosmtamayo@gmail.com>
2019-09-05 03:19:21 -0400

    Working commit

--- script.js
+++ script.js
@@ -1,57 +1,72 @@
 'use strict';
 
-const apiKey = '1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV';
-const searchURL = 'https://developer.nps.gov/api/v1';
-
-function formatParams(params) {
-  const queryItems = Object.keys(params)
-  .map(key -> `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(key => `${[encodeURIComponent(key)]}=${encodeURIComponent(params[key])}`);
+    return queryItems.join('&');
 }
 
-function getParks() {
-  const params = {
-    key: apiKey,
-    q: query,
-    part: '?',
-    maxResults
-  };
+function showParks(responseJson, maxResults) {
+    console.log(responseJson);
 
-  const queryString = formatParams(params)
-  const url = searchURL + '?' + queryString;
+    $('.error-message').empty();
+    $('.results-list').empty();
 
-  fetch(url)
-    .then(response => {
-      if(response.ok) {
-        return (response.json);
-      }
-      throw new Error(reponse.statusText);
-    })
-
-    .then(responseJson => console.log())
-    ,catch(err => {
-      $('#js-error-message').text(`"This is embarassing. ${err.message}Something's not right."`)
-    });
-
+    for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+        $('.results-list').append(`<li>
+      <h3>${responseJson.data[i].fullName}</h3>
+      <a href="${responseJson.data[i].url}" target="blank"><p>${responseJson.data[i].url}</p></a>
+      <p>${responseJson.data[i].description}</p>
+      <p>${responseJson.data[i].directionsInfo} <a href="${responseJson.data[i].directionsUrl}" target="blank">Click for Directions</a></p>
+      
+      </li><br><br>`);
+    }
+    $('#results').removeClass('hidden');
 }
+
+function getParks(baseUrl, stateArr, maxResults, apiKey) {
+
+    const params = {
+        stateCode: stateArr,
+        limit: maxResults
+    }
+
+    const queryString = formatQueryParams(params);
+    const url = baseUrl + '?' + queryString + '&api_key=' + apiKey;
+    console.log(url);
+
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => showParks(responseJson, maxResults))
+        .catch(err => {
+            $('.js-error').text(`Something went wrong: ${err.message}`);
+        });
+}
+
 
 function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    const searchTerm = $('js-search').val();
-    const maxResults = $('js-max-results').val();
-    console.log('watchform working');
-    getParks();
-  });
+    $('.js-form').on('submit', function () {
+        event.preventDefault();
+        const baseUrl = 'https://developer.nps.gov/api/v1/parks'
+        const stateArr = $('#js-state').val().split(",");
+        const maxResults = $('#numResults').val();
+
+        const apiKey = '1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV'
+        getParks(baseUrl, stateArr, maxResults, apiKey);
+    })
 }
 
 $(watchForm);
 
-
-
+// ----------------------------------------INSTRUCTION--------------------------------------------------
 
 /* SAMPLE:
-https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV */ 
+https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=1AX6CKlRzxx3D4RjipBxDmi6sN46rhmWBIif60IV */
 
 // Review The National Parks Services API documentation and create an API key.
 
@@ -74,10 +89,6 @@ https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=1AX6CKlRzxx3D4RjipB
 // -------------------------------------EXTRA CREDIT
 // As a stretch goal, try adding the park's address to the results. (directionsInfo)
 
-
-
-
-
 // -------------------------------------API DOCUMENTATION
 // Each API request contains:
 // Resource Endpoint
@@ -92,4 +103,4 @@ Resource Endpoint - https://developer.nps.gov/api/v1/alerts
 Query String Parameters - parkCode=acad,dena
 However, just pasting that URL in a browser address bar won't return any results as you also need to send your API key in the HTTP request header. In order to learn more about that, be sure to read the API Guides page about authentication. Also, test out queries and learn more about the API in the API Documentation section of this website.
 
-Good luck with your programming project and thanks for using NPS data. */ 
+Good luck with your programming project and thanks for using NPS data. */
